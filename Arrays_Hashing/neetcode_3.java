87813401a3d0188a62812438c83b0806144c4f74
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-03-01 23:17:20 +0530

    neetcode 3

--- Arrays_Hashing/neetcode_3.java
+++ Arrays_Hashing/neetcode_3.java
@@ -13,6 +13,7 @@ public class neetcode_3 {
     }
 
     public static int[] twosum(int[] nums, int target) {
+        
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int compliment = target - nums[i];
