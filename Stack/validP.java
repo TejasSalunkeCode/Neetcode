7611eeafc29b301c4b6a46ae41be768ed4b4dbd3
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-06-11 19:35:14 +0530

    valid paranthesis

---
+++ Stack/validP.java
@@ -0,0 +1,38 @@
+package Stack;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class validP {
+    
+    public static void main(String[] args) {
+        String str="()[]{}";
+        System.out.println(isValid(str));
+    }
+    public static boolean isValid(String s) {
+       
+        HashMap<Character,Character> mappBrackets = new HashMap<>();
+        Stack<Character> s1 = new Stack<>();
+
+        mappBrackets.put(')', '(');
+        mappBrackets.put('}', '{');
+        mappBrackets.put(']', '[');
+
+        for (int i = 0; i < s.length(); i++) {
+            char charct=s.charAt(i);
+            if(!mappBrackets.containsKey(charct)){
+                s1.push(charct);
+            }else{
+                if(s1.isEmpty()){
+                    return false;
+                }
+                char topEle=s1.pop();
+                if(topEle!=mappBrackets.get(charct)){
+                    return false;
+                }
+            }
+        }
+        return s1.isEmpty();
+
+    }
+}
